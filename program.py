96f20b9ac97bde14105f7d31ccb745bdf5ef473c
Solinas95 <100536871+Solinas95@users.noreply.github.com>
2023-05-08 03:31:24 +0200

    Update program.py

--- program.py
+++ program.py
@@ -166,21 +166,4 @@ if create_sequences_button and train_data_file is not None:
       
 
 
-    # Build and display the model
-    build_model_button = st.button("Build Model")
-
-    # Create train and validation arrays
-    # Model parameters
-    num_lstm_layers = st.sidebar.slider("Number of LSTM layers", min_value=1, max_value=2, value=1, step=1)
-    activation_function = st.sidebar.selectbox("Activation function", options=["tanh", "relu"])
-    optimizer = st.sidebar.selectbox("Optimizer", options=["RMSprop", "adam"])
-    weight_initializer = st.sidebar.selectbox("Weight initializer", options=["glorot_uniform", "he_uniform"])
-    regularization_l1 = st.sidebar.number_input("L1 regularization", min_value=0.0, max_value=1.0, value=0.0, step=0.01)
-    regularization_l2 = st.sidebar.number_input("L2 regularization", min_value=0.0, max_value=1.0, value=0.0, step=0.01)
-    layer_normalization = st.sidebar.checkbox("Layer normalization")
-    batch_normalization = st.sidebar.checkbox("Batch normalization")
-
-input_shape = (X_train.shape[0], X_train.shape[1])
-model = build_lstm_model(input_shape, num_lstm_layers, activation_function, optimizer, weight_initializer, regularization_l1, regularization_l2, layer_normalization, batch_normalization)
-st.write(model.summary())
 
